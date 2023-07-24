79cbf433dc9e2ed5c759598d22d6785672d8898f
ShBekzodbek <shoyatbekov03032003@gmail.com>
2023-07-24 08:51:01 +0500

    1.0.0

--- main.go
+++ main.go
@@ -1,8 +1,9 @@
 package main
 
 import (
-	src "Need_for_speed_1.0.0/src"
+	"Need_for_speed_1.0.0/src"
 	"fmt"
+
 	"sync"
 	"time"
 )
@@ -13,18 +14,17 @@ func main() {
 	src.Start("Game Started")
 	time.Sleep(time.Second * 2)
 	src.Clean()
-	src.Start("Choose a level you want")
+	src.Start("Choose a level")
 	var level int
 	_, err := fmt.Scan(&level)
 	if err != nil {
 		panic(err)
 		return
 	}
-
 	var move int
 	go src.Race(level, &move, &wg)
 	go src.GetInput(&move, &wg)
-
 	wg.Wait()
+	//TODO to complete a last task that when game finished asked whether user wants to play or not
 
 }
